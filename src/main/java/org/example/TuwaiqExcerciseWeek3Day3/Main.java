9b0fa17974debdd33b5ca2df0f2bcd0da5a352a0
ammaryy <ammar.yagoub3@gmail.com>
2025-10-26 14:53:30 +0300

    Encapsulation exercise finished

--- src/main/java/org/example/TuwaiqExcerciseWeek3Day3/Main.java
+++ src/main/java/org/example/TuwaiqExcerciseWeek3Day3/Main.java
@@ -6,22 +6,26 @@ public class Main
 
         //_______________________________Q-1_________________________________\\
 
-        Account account1 = new Account("1", "Ammar Yagoub");
-        Account account2 = new Account("2", "Yousef Yagoub");
-        account1.setBalance(account1.credit(1000));
-        account1.setBalance(account1.debit(2000));
-        System.out.println(account1.getBalance());
-        account2.setBalance(account1.transferTo(account2, 100));
-        account1.setBalance(account2.transferTo(account1, 10000));
-        System.out.println(account1);
-        System.out.println(account2);
+//        Account account1 = new Account("1", "Ammar Yagoub");
+//        Account account2 = new Account("2", "Yousef Yagoub");
+//        account1.setBalance(account1.credit(1000));
+//        account1.setBalance(account1.debit(2000));
+//        System.out.println(account1.getBalance());
+//        account2.setBalance(account1.transferTo(account2, 100));
+//        account1.setBalance(account2.transferTo(account1, 10000));
+//        System.out.println(account1.getName());
+//        account1.setName("AmmarYagoub");
+//        System.out.println(account1);
+//        System.out.println(account2);
 
 
         //_______________________________Q-2_________________________________\\
 
         Employee e1 = new Employee("1","Ammar Yagoub",6500);
+        e1.setSalary(12000);
         e1.setSalary(e1.raisedSalary(10));
         System.out.println("your annual salary is "+e1.getAnnualSalary());
         System.out.println(e1);
+
     }
 }
